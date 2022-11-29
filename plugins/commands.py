3dd060ec58fc43dd73afe201dd04173b31f0dad6
ᴀᴋꜱʜᴀy <101881271+Luttappi333@users.noreply.github.com>
2022-11-29 22:21:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,10 +22,10 @@ BATCH_FILES = {}
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
-                    InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('⚡️ᴀᴅᴅ ᴍᴇ ᴛᴏ yᴏᴜʀ ɢʀᴏᴜᴩ⚡️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                   ],[
-                    InlineKeyboardButton('💿SUᎮᎮԾᏒϮ💿', url=GRP_LNK), 
-                    InlineKeyboardButton('🤠ΛBOUT🤠', url=f"https://t.me/{temp.U_NAME}")
+                    InlineKeyboardButton('💿ꜱᴜᴩᴩᴏʀᴛ💿', url=GRP_LNK), 
+                    InlineKeyboardButton('🤠ᴀʙᴏᴜᴛ🤠', url=f"https://t.me/{temp.U_NAME}")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
@@ -40,10 +40,10 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('⚡️ᴀᴅᴅ ᴍᴇ ᴛᴏ yᴏᴜʀ ɢʀᴏᴜᴩ⚡️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                  ],[
-                    InlineKeyboardButton('🦋SΞΛᏒCH🦋', switch_inline_query_current_chat=''), 
-                    InlineKeyboardButton('🦋SUᎮᎮԾᏒϮ🦋', url=GRP_LNK)
+                    InlineKeyboardButton('⚠️ꜱᴇᴀʀᴄʜ⚠️', switch_inline_query_current_chat=''), 
+                    InlineKeyboardButton('🦋ꜱᴜᴩᴩᴏʀᴛ🦋', url=GRP_LNK)
                  ],[
                     InlineKeyboardButton('♻️𝐇𝐞𝐥𝐩♻️', callback_data='help'),
                     InlineKeyboardButton('♻️𝐀𝐛𝐨𝐮𝐭♻️', callback_data='about')
@@ -89,10 +89,10 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('⚡️ᴀᴅᴅ ᴍᴇ ᴛᴏ yᴏᴜʀ ɢʀᴏᴜᴩ⚡️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                  ],[
-                    InlineKeyboardButton('🦋SΞΛᏒCH🦋', switch_inline_query_current_chat=''), 
-                    InlineKeyboardButton('🦋SUᎮᎮԾᏒϮ🦋', url=GRP_LNK)
+                    InlineKeyboardButton('🦋ꜱᴇᴀʀᴄʜ🦋', switch_inline_query_current_chat=''), 
+                    InlineKeyboardButton('🦋ꜱᴜᴩᴩᴏʀᴛ🦋', url=GRP_LNK)
                  ],[
                     InlineKeyboardButton('♻️𝐇𝐞𝐥𝐩♻️', callback_data='help'),
                     InlineKeyboardButton('♻️𝐀𝐛𝐨𝐮𝐭♻️', callback_data='about')
